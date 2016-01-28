0ed87d61e684bd6a4b6d546c2dc5d7917aacc172
Daniel Riley <driley3@gmail.com>
2016-01-28 17:15:11 -0500

    third commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # Change these
-server 'your_server_ip', port: your_port_num, roles: [:web, :app, :db], primary: true
+server '104.131.54.9', port: 80, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@example.com:username/appname.git'
-set :application,     'appname'
+set :repo_url,        'git@github.com:driley3/hoppernaturals.git'
+set :application,     'hoppernaturals'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
